d3ce58d6ee442ac5dcc0c3160824d2b5ccbfb3d2
Mehdi6t6 <mehdi.rasoulnia@gmail.com>
2024-11-06 13:24:13 +0330

    change LOGO

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
 
     <nav class="navbar navbar-expand-lg bg-white">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="./assets/image/Group 2.png" style="width: 150px;" alt=""></a>
+            <a class="navbar-brand" href="#"><img src="./assets/image/BLUESOURCEIR.svg" style="width: 150px;" alt=""></a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"

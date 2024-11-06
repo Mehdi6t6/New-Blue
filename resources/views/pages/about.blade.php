bd3dc45474789642428750893ffeb2961e9b6a22
Mehdi6t6 <mehdi.rasoulnia@gmail.com>
2024-11-06 13:11:11 +0330

    change place pic

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,67 +2,69 @@
 
 @section('content')
 
-
-<div class="container">
-    <div class="row">
-        <div class="col-10">
-            <h3 class="styltext1"> بلوسورس</h3>
-            <p style="color: #004187;">جهت ثبت سفارش با شماره تماس 09127305627 تماس حاصل فرمایید.
-            </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h3 class="styltext1"> بلوسورس</h3>
+                <p style="color: #004187;">جهت ثبت سفارش با شماره تماس 09127305627 تماس حاصل فرمایید.
+                </p>
+            </div>
+            <div class="col-2 d-none d-md-block"><img src="./assets/image/questions 1.png" alt=""></div>
         </div>
-        <div class="col-2 d-none d-md-block"><img src="./assets/image/questions 1.png" alt=""></div>
     </div>
-</div>
-<div class="container">
-    <div class="row">
+    <div class="container">
+        <div class="row">
 
-        <div class="col-md col-12 text-center bg-white rounded-4 m-2">
-            <div class="p-3"><img src="./assets/image/personal/2س1.jpg" class="img-fluid bg-info " alt=""> </div>
-            <div><p class="MedumText" >مهراد رضایی</p>
-                <p class="SmallTxt">گرافیست وطراح رابط کاربری</p>
+
+            <div class="col-md col-12 text-center bg-white rounded-4 m-2">
+                <div class="p-3"><img src="./assets/image/personal/45.jpg" class="img-fluid bg-info " alt=""></div>
+                <div><p class="MedumText"> آرش میرزایی</p>
+                    <p class="SmallTxt">برنامه نویس (Full Stack)</p>
+                </div>
             </div>
-        </div>
-        <div class="col-md col-12 text-center bg-white rounded-4 m-2">
-            <div class="p-3"><img src="./assets/image/personal/photo_2024-11-04_13-07-50.jpg" class="img-fluid bg-info " alt=""> </div>
-            <div>
-                <p class="MedumText" >علیرضا مدنی </p>
-                <p class="SmallTxt"> برنامه نویس (Android)</p>
+            <div class="col-md col-12 text-center bg-white rounded-4 m-2">
+                <div class="p-3"><img src="./assets/image/personal/98.jpg" class="img-fluid bg-info " alt=""></div>
+                <div><p class="MedumText"> وحید دانشمند </p>
+                    <p class="SmallTxt">متخصص شبکه و مجازی سازی </p>
+                </div>
             </div>
-        </div>
-        <div class="col-md col-12 text-center bg-white rounded-4 m-2">
-            <div class="p-3"><img src="./assets/image/personal/45.jpg" class="img-fluid bg-info " alt=""> </div>
-            <div><p class="MedumText"> آرش میرزایی</p>
-                <p class="SmallTxt">برنامه نویس (Full Stack)</p>
+
+
+            <div class="col-md col-12 text-center bg-white rounded-4 m-2">
+                <div class="p-3"><img src="./assets/image/personal/photo_2024-11-04_13-07-50.jpg"
+                                      class="img-fluid bg-info " alt=""></div>
+                <div>
+                    <p class="MedumText">علیرضا مدنی </p>
+                    <p class="SmallTxt"> برنامه نویس (Android)</p>
+                </div>
             </div>
-        </div>
-        <div class="col-md col-12 text-center bg-white rounded-4 m-2">
-            <div class="p-3"><img src="./assets/image/personal/98.jpg" class="img-fluid bg-info " alt=""> </div>
-            <div><p class="MedumText"> وحید دانشمند </p>
-                <p class="SmallTxt">متخصص شبکه و مجازی سازی </p>
+
+            <div class="col-md col-12 text-center bg-white rounded-4 m-2">
+                <div class="p-3"><img src="./assets/image/personal/mehdi2.jpg" class="img-fluid bg-info " alt=""></div>
+                <div><p class="MedumText">مهدی رسول نیا</p>
+                    <p class="SmallTxt">برنامه نویس (Front End)</p>
+                </div>
             </div>
-        </div>
-        <div class="col-md col-12 text-center bg-white rounded-4 m-2">
-            <div class="p-3"><img src="./assets/image/personal/mehdi2.jpg" class="img-fluid bg-info " alt=""> </div>
-            <div><p class="MedumText">مهدی رسول نیا</p>
-                <p class="SmallTxt">برنامه نویس (Front End)</p>
+            <div class="col-md col-12 text-center bg-white rounded-4 m-2">
+                <div class="p-3"><img src="./assets/image/personal/photo_2024-11-05_09-23-29.jpg"
+                                      class="img-fluid bg-info " alt=""></div>
+                <div><p class="MedumText">عرفان طباطبایی </p>
+                    <p class="SmallTxt"> خدمات پسیو شبکه</p>
+                </div>
+
+
             </div>
-        </div>
-        <div class="col-md col-12 text-center bg-white rounded-4 m-2">
-            <div class="p-3"><img src="./assets/image/personal/photo_2024-11-05_09-23-29.jpg" class="img-fluid bg-info " alt=""> </div>
-            <div><p class="MedumText">عرفان طباطبایی </p>
-                <p class="SmallTxt"> خدمات پسیو شبکه</p>
+            <div class="col-md col-12 text-center bg-white rounded-4 m-2">
+                <div class="p-3"><img src="./assets/image/personal/2س1.jpg" class="img-fluid bg-info " alt=""></div>
+                <div><p class="MedumText">مهراد رضایی</p>
+                    <p class="SmallTxt">گرافیست وطراح رابط کاربری</p>
+                </div>
             </div>
+
+
         </div>
-
-
-
-
-
-
 
     </div>
-
-</div>
 
 @endsection
 
